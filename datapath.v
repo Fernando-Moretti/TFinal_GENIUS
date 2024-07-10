73a94fddca5647c0f910283cfe86caf07b5b6296
Fernando Moretti <130860907+Fernando-Moretti@users.noreply.github.com>
2024-07-10 19:33:47 -0300

    Add files via upload
    ERRO NA SIMULAÇÃO

--- datapath.v
+++ datapath.v
@@ -20,20 +20,23 @@ module datapath(
 	end_User, 
 	end_time, 
 	win, 
-	match
+	match,
+	SEQFPGA
 );
 
 
 input wire CLOCK_50, R1, R2, E1, E2, E3, E4, SEL;
 input wire [3:0] KEY; 
-input wire [9:0] SWITCH;
+input wire [7:0] SWITCH;
 
 output wire [3:0] leds;
 output wire [6:0] hex0, hex1, hex2, hex3, hex4, hex5;
 output wire end_FPGA, end_User, end_time, win, match;
+output wire[3:0] SEQFPGA;
 
 wire [7:0]SETUP;
 wire w_win;
+assign win = w_win;
 wire [6:0]w_mux2x1_hex5;
 wire [6:0]w_mux2x1_hex4;
 wire [6:0]w_mux2x1_hex3;
@@ -41,16 +44,34 @@ wire [6:0]w_mux2x1_hex2;
 wire [6:0]w_mux2x1_hex1;
 wire [6:0]w_mux2x1_hex0;
 
-wire [3:0]NKEY;
-assign NKEY[3:0] = ~KEY[3:0];
+//buttosnync
+wire [3:0] NBTN;
+//NBTN INSTANCIADO NO COUNTER USER
+wire b1, b2, b3, b4;
 
-wire [9..6] LEDR1;
-wire [3..0] LEDR2;
+
+
+ButtonSync buttonSYNC(
+.KEY0(KEY[0]), .KEY1(KEY[1]), .KEY2(KEY[2]), .KEY3(KEY[3]), .CLK(CLOCK_50),
+	.BTN0(b1), .BTN1(b2), .BTN2(b3), .BTN3(b4)
+
+);
+assign NBTN[0] = ~b1;
+assign NBTN[1] = ~b2;
+assign NBTN[2] = ~b3;
+assign NBTN[3] = ~b4;
+
+
+//COMP
+ wire [63:0]OUT_USER;
+ wire [63:0]OUT_FPGA;
+ wire END_User;
+ assign match = ((OUT_FPGA == OUT_USER) & END_User) ? 1'b1 : 1'b0;
 
 //hex5
 mux2x1 HEX5_1(
-	.a_i(7'b100_0111), //F
-	.b_i(7'b011_1110), //U 
+	.a_i(7'b101_1011), //F
+	.b_i(7'b100_0111), //U 
 	.sel_i(w_win),
 	.d_o(w_mux2x1_hex5[6:0])
 );
@@ -61,8 +82,9 @@ mux2x1 HEX5_2(
 	.sel_i(SEL),
 	.d_o(hex5[6:0])
 );
-
+  
 //hex4
+
 wire [6:0]w_hex4_dec;
 mux2x1 HEX4_2(
 	.a_i(7'b110_0111), //P 
@@ -71,16 +93,16 @@ mux2x1 HEX4_2(
 	.d_o(w_mux2x1_hex4[6:0])
 );
 
+segDisplay HEX4_DISPLAY(
+    .b({2'b00,SETUP[7:6]}),    //'00' & setup[7:6]
+    .d(w_hex4_dec[6:0])
+);
+
 mux2x1 HEX4_1(
 	.a_i(w_hex4_dec[6:0]), 
 	.b_i(w_mux2x1_hex4[6:0]),
 	.sel_i(SEL),
 	.d_o(hex4[6:0])
-);
-
-segDisplay HEX4_DISPLAY(
-    .b({2'b00,SETUP[7:6]}),    //'00' & setup[7:6]
-    .d(w_hex4_dec[6:0])
 );
 
 //hex3
@@ -102,10 +124,15 @@ mux2x1 HEX3_2(
 wire [3:0]TIME;
 wire [6:0]w_hex2_dec;
 mux2x1 HEX2_1(
-	.a_i(7'b111_0111),  //A
-	.b_i(7'b000_0101), //r
+	.a_i(7'b110_0111),  //A
+	.b_i(7'b101_1011), //r
 	.sel_i(w_win),
 	.d_o(w_mux2x1_hex2[6:0])
+);
+
+segDisplay HEX2_DISPLAY(
+    .b(TIME[3:0]),  //TIME
+    .d(w_hex2_dec[6:0])
 );
 
 mux2x1 HEX2_2(
@@ -113,11 +140,6 @@ mux2x1 HEX2_2(
 	.b_i(w_mux2x1_hex2[6:0]),
 	.sel_i(SEL),
 	.d_o(hex2[6:0])
-);
-
-segDisplay HEX2_DISPLAY(
-    .b(TIME[3:0]),  //TIME
-    .d(w_hex2_dec[6:0])
 );
 
 //hex1
@@ -138,13 +160,13 @@ segDisplay HEX1_DISPLAY(
 //hex0
 wire [6:0]w_hex0_dec;
 wire [6:0]w_hex0_dec_2;
+wire [3:0]ROUND;
 mux2x1 HEX0(
 	.a_i(w_hex0_dec_2[6:0]), 
 	.b_i(w_hex0_dec[6:0]), 
 	.sel_i(SEL),     
 	.d_o(hex0[6:0])
 );
-
 segDisplay HEX0_DISPLAY1(
     .b(ROUND[3:0]), //round
     .d(w_hex0_dec_2[6:0])
@@ -156,146 +178,127 @@ segDisplay HEX0_DISPLAY2(
 );
 
 // COUNTERS
-wire [2:0] w_TEMPO;
-wire w_end_time;
-wire [3:0]ROUND;
-
-
 counterGENERAL Timer(
 	 .CLKT(CLOCK_50), 
 	 .R(R2), 
 	 .E(E2), 
-	 .CONTADOR(),
-	 .REGISTRADOR(),
-	 .data()
+	 .CONTADOR(TIME),
+	 .REGISTRADOR(end_time),
+	 .data(4'b1010)
+	 
 );
-counterGENERAL Round(
+
+counterGENERAL Round(
 .data(SETUP[3:0]),
 .R(R1),
 .E(E4),
 .CLKT(CLOCK_50),
-.CONTADOR(ROUND)
+.CONTADOR(ROUND[3:0]),
+.REGISTRADOR(win)
 
 );
-wire w_end_user;
-assign w_end_user = end_User;
-wire w_E;
+
+
+wire [3:0] SEQFPGA_seq;
+
+counterGENERAL FPGA(
+.CLKT(CLOCK_50),
+.R(R2),
+.E(E3),
+.data(ROUND[3:0]),
+.CONTADOR(SEQFPGA_seq[3:0]),
+.REGISTRADOR(end_FPGA)
+);
+
 
 wire and1;
 wire or1;
-
 and (and1,or1,E2);
-or (or1,NBTN,or1);
+or (or1,NBTN[0],NBTN[1],NBTN[2],NBTN[3]);
 
+wire [3:0]SEQUSER;
 counterGENERAL User(
 .data(ROUND[3:0]),
 .R(R2),
 .E(and1), //fazer uma wire com um processo de AND E OR
 .CLKT(CLOCK_50),
-.REGISTRADOR(w_end_user)
-);
-wire w_end_FPGA;
-assign w_end_FPGA = end_FPGA;
-
-wire [3:0]SEQFPGA;
-counterGENERAL FPGA(
-.CLKT(CLKHZ),
-.R(R2),
-.E(E3),
-.data(ROUND[3:0]),
-.CONTADOR(SEQFPGA[3:0]),
-.REGISTRADOR(w_end_FPGA)
+.REGISTRADOR(end_user),
+.CONTADOR(SEQUSER)
 );
 
-wire [5:0] SETUPMUX;
-mux4x1_4 muxFPGA(
- .SEL(SETUPMUX[5:4]),
- .ENT0(SEQMUX00),
- .ENT1(SEQMUX01),
- .ENT2(SEQMUX10),
- .ENT3(SEQMUX11),
- .out(SEQFPGA)
-);
 
-wire SEQMUX00;
+wire [3:0]SEQMUX00;
+wire [3:0]SEQMUX01;
+wire [3:0]SEQMUX10;
+wire [3:0]SEQMUX11;
+wire [5:0]SETUPMUX;
+
 decSeq00 SEQ1(
- .address(SEQFPGA),
- .saida(SEQMUX00)
+ .address(SEQFPGA_seq[3:0]),
+ .saida(SEQMUX00[3:0])
 );
 
-wire SEQMUX01;
 decSeq01 SEQ2(
- .address(SEQFPGA),
- .saida(SEQMUX01)
+ .address(SEQFPGA_seq[3:0]),
+ .saida(SEQMUX01[3:0])
 );
 
-wire SEQMUX10;
 decSeq10 SEQ3(
- .address(SEQFPGA),
- .saida(SEQMUX10)
+ .address(SEQFPGA_seq[3:0]),
+ .saida(SEQMUX10[3:0])
 );
 
-wire SEQMUX11;
 decSeq11 SEQ4(
- .address(SEQFPGA),
- .saida(SEQMUX11)
+ .address(SEQFPGA_seq[3:0]),
+ .saida(SEQMUX11[3:0])
  
 );
-//LOGICA
-LOGICA Logica(
-    .REG_SetupLEVEL(SETUP[7:6]), 
-    .ROUND(ROUND[3:0]),          
-    .POINTS(POINTS[7:0]),
-	 .REG_SetupMAPA(SETUP[5:4])
+wire [3:0]SEQ_FPGA;
+mux4x1_4 muxFPGA(
+ .SEL(SETUPMUX[5:4]),
+ .ENT0(SEQMUX00[3:0]),
+ .ENT1(SEQMUX01[3:0]),
+ .ENT2(SEQMUX10[3:0]),
+ .ENT3(SEQMUX11[3:0]),
+ .out(SEQ_FPGA[3:0])
 );
+
 //REG_USER
-REG_Setup regSetup(
-    .clk(CLOCK_50),
-    .R(R1),           
-    .E(R1),           
-    .SW(SWITCH[9:2]),     
-    .setup(SETUP[7:0])
-);
+
+wire and2;
+wire or2;
+
+or(or2,NBTN[0],NBTN[1],NBTN[2],NBTN[3]);
+and(and2,or2,E2);
+REG_User regUSER(
+	 .clk(CLOCK_50),
+    .R(R2),           
+    .E(and2),          
+    .data({NBTN[3:0], OUT_USER[63:4]}),  
+    .q(OUT_USER[63:0])  
+);	
+
+
 //REG_FPGA
-wire [63:0]OUT_FPGA;
-wire [63:0]SUM_DATA;
-assign SUM_DATA[63:0] = {SEQFPGA[3:0], OUT_FPGA[63:4]}; //COMBINAÇÃO DESSES SINAIS EM UM UNICO VETOR
+wire CLKHZ;
 REG_FPGA regFPGA(
     .clk(CLKHZ),
     .R(R2),           
     .E(E3),          
-    .data(SUM_DATA[63:0]),  
+    .data({SEQ_FPGA[3:0],OUT_FPGA[63:4]}),  
     .q(OUT_FPGA[63:0]),  
     .q3()  
 
 );
 
-//buttosnync
-wire [3:0]BTN;
-wire [3:0]NBTN;
-assign NBTN[3:0] = ~BTN[3:0];
-
-//REG_USER
-wire [63:0]OUT_USER;
-wire and2;
-wire or2;
-
-or(or2,NBTN,or2);
-and(and2,E2,or2);
-
-wire [63:0]SUM_USER;
-assign SUM_USER[63:0] = {NBTN[3:0], OUT_USER[63:4]};
-
-REG_User regUSER(
-	 .clk(CLOCK_50),
-    .R(R2),           
-    .E(and2),          
-    .data(SUM_USER[63:0]),  
-    .q(OUT_USER[63:0])  
-);	
-//COMP
- and (match, (OUT_FPGA == OUT_USER), end_User);
- 
+//REG_Setup
+REG_Setup regSetup(
+    .clk(CLOCK_50),
+    .R(R1),           
+    .E(R1),           
+    .SW(SWITCH[7:0]),   //9:2  
+    .setup(SETUP[7:0])
+);
  //FSM CLOCK 
  wire clk_25,clk_05,clk_1,clk_2;
  FSM_clock clock_FSM(
@@ -306,8 +309,31 @@ REG_User regUSER(
     .C1Hz(clk_1),
     .C2Hz(clk_2)
  );
+ 
+ mux4X1_1 muxFSM(
+     .level(SETUP[7:6]),     
+    .CL1(clk_25),            
+    .CL2(clk_05),             
+    .CL3(clk_1),             
+    .CL4(clk_2),             
+    .CLKHZ(CLKHZ)   
+ );
 
-	wire [63..60]
+//LOGICA
+LOGICA Logica(
+    .REG_SetupLEVEL(SETUP[7:6]), 
+    .ROUND(ROUND[3:0]),          
+    .POINTS(POINTS[7:0]),
+	 .REG_SetupMAPA(SETUP[5:4])
+);
 
-	
+//LEDR
+wire [3:0]LEDR_key;
+assign LEDR_key[0] = ~OUT_FPGA;
+assign LEDR_key[1] = ~OUT_FPGA;
+assign LEDR_key[2] = ~OUT_FPGA;
+assign LEDR_key[3] = ~OUT_FPGA;
+ 
+assign LEDS = {OUT_FPGA[63:60], LEDR_key[3:0]};
+
 endmodule
